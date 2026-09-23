1b159ee1049bd56b3e4d5cdf573f61a3c65343ea
Alvaro <ag@allyy.io>
2026-09-23 17:26:37 +0200

    feat: install Mantine v7 and configure dark-theme provider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,17 @@
 import "./i18n/index";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./index.css";
+import App from "./App.tsx";
+import { theme } from "./theme.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ColorSchemeScript defaultColorScheme="dark" />
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
